0314b4edb8faa61e13b16e567f0278a641fa6d79
IvanTregub <tregiven@gmail.com>
2021-11-02 00:44:56 +0400

    add TrimSpace, ParseFloat

--- pass_fail.go
+++ pass_fail.go
@@ -5,16 +5,30 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Print("Enter a grade: ")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
+	fmt.Print("Enter a grade: ")          // запрашиваем значение
+	reader := bufio.NewReader(os.Stdin)   // чтение ввода с клавиатуры
+	input, err := reader.ReadString('\n') // читаем вводимые данные
 	if err != nil {
 		log.Fatal(err)
-
 	}
 	fmt.Println(input)
+
+	input = strings.TrimSpace(input)            // удаляем символ новый строки
+	grade, err := strconv.ParseFloat(input, 64) // преобразование введеной строки в float64
+	if err != nil {
+		log.Fatal(err)
+	}
+	var status string // область видимости переменной в границах функции
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("A grade of", grade, "is", status)
 
 }
